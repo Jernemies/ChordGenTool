dfd9bb366369527e6e40faf49a5c10c360dffabd
Jernemies <jklaavo@edu.lapinamk.fi>
2024-01-23 20:33:17 +0200

    Layout vaiheeseen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/material.dart';
 import 'chord.dart';
 import 'generate.dart';
+
 
 /*
 https://api.dart.dev/stable/3.2.4/dart-math/Random-class.html
@@ -41,7 +43,7 @@ class AboutPage extends StatelessWidget {
               margin: const EdgeInsets.all(10),
               padding: const EdgeInsets.all(20),
               alignment: Alignment.center,
-              child: const Text('This tool generates random chord progressions. Without extensions you get 7th chords. With extensions you get 9th, 11th, and 13th chords. Chord voicings are not specified, so feel free to voice them however you like.')
+              child: const Text("This tool generates random chord progressions. Chords are expressed as 7th chords, with extensions specified separately should you want thme. Chord voicings are not specified, so feel free to voice them however you like. Keys are not specified at the moment, so you'll have to transpose them to any key you like.")
             ),
           ),
         ),
@@ -58,6 +60,10 @@ class GeneratePage extends StatefulWidget {
 }
 
 class _GeneratePageState extends State<GeneratePage> {
+  int chords = 4;
+  String chordsText = '';
+  String extensionsText = '(9,11,13)';
+  List<Chord> chordList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,22 +71,42 @@ class _GeneratePageState extends State<GeneratePage> {
           title: const Text('Generate'),
           backgroundColor: Colors.red,
         ),
-        body: Center(
-          child: SizedBox.expand(
-            child: Container(
-              alignment: Alignment.center,
-              child: Text('Hello World'),
-              margin: const EdgeInsets.all(10),
-              padding: const EdgeInsets.all(20),
+          body: Center(
+          child: Column(
+          children: [
+            Container(
+              height: 160,
+              child: Container(
+                alignment: Alignment.center,
+                height: 160,
+                child: SpinBox(
+                min: 1,
+                max: 16,
+                step: 1,
+                value: chords.toDouble(),
+                onChanged: (value) => setState(() => chords = value.toInt()),
+                decoration: const InputDecoration(labelText: 'Number of Chords'),),
+              ),
             ),
-          ),
-        ),
+            Container(
+              //For looppi, jolla luodaan joka soinnulle oma container
+              alignment: Alignment.centerLeft,
+              child: Text(chordsText.toString(), style: const TextStyle(fontSize: 80),),
+            ),]
+        ),),
         floatingActionButton: Container(
           height: 80,
           width: 150,
           child: FloatingActionButton(
             onPressed: () {
-              print('Generated');
+              chordList = generate(chords, true);
+              for (int i = 0; i < chords; i++) {
+                  String chordName = chordList[i].name.toString();
+                  chordsText += chordName;
+                }
+              setState(() {
+                chordsText;
+              });
             },
           backgroundColor: Colors.red,
           child: const Text('Generate'),
